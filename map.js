dcbe17a9e73ef1506445b9fed93a939ef10c9d75
William Santos Silvsa <willsgq@gmail.com>
2021-01-30 14:13:59 -0300

    Teste map

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 let numbers = [1, 4, 5, 9, 14, 21]
 
 let dobraNumbers = numbers.map( function(a){
-    return a * 
+    return a * 3
 
 } )
 
 console.log (dobraNumbers)
-
+                                                                                                   
